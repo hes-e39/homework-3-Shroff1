24c0333f5ed3689f54122c92fb6c16bf271bc0dd
Shroff1 <36019578+Shroff1@users.noreply.github.com>
2024-09-29 14:49:12 -0400

    colorUpdated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import colors from './colors.json';
+
 const App = () => {
-    return <h1>Hello World</h1>;
+    return <h1 style={{ color: `#${colors[0].hex}` }}>Hello World</h1>;
 };
 
 export default App;
